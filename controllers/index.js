f595d5560db3d2b8dd89fe06bf6926590707b2aa
Kyle Vermeer <kvermeer@stanford.edu>
2013-02-12 23:38:18 +0000

    Added jade views

--- controllers/index.js
+++ controllers/index.js
@@ -1,4 +1,4 @@
 module.exports = function(req,res) {
     res.type('.html');
-    res.send('<!DOCTYPE><html><head><title>Kyle</title></head><body><h1>Hello World!</h1></body></html>');
+    res.render('index');
 }
