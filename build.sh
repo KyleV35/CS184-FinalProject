0b0f19790b43857c87990c3bdb8df25d12b61c34
Kyle Vermeer <kvermeer@stanford.edu>
2013-02-13 13:33:44 +0000

    Added some new images

--- build.sh
+++ build.sh
@@ -1,4 +1,8 @@
+#Compile bootstrap CSS
 cd bootstrap
 lessc ./less/bootstrap.less > ../public/stylesheets/bootstrap.css
 lessc --compress ./less/bootstrap.less > ../public/stylesheets/bootstrap.min.css
 cd ..
+#Now in top level directory
+lessc ./less/main.less > ./public/stylesheets/main.css
+lessc --compress ./less/main.less > ./public/stylesheets/main.min.css
